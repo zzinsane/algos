dede91867abbec7fb5084f3b1f53413cc835b39c
zhangfq <zhangfqjs@163.com>
2017-02-13 01:35:21 +0800

    reverse list by k group init commit

---
+++ ReverseNodeK.py
@@ -0,0 +1,106 @@
+# Definition for singly-linked list.
+class ListNode(object):
+	def __init__(self, x):
+		self.val = x
+		self.next = None
+
+class Solution(object):
+	def print_all(self, l):
+		array = []
+		a = l
+		while a:
+			array.append("%s" %a.val)
+			a = a.next
+		print "==>".join(array)
+
+	def reverse(self, head):
+
+		before, after = head, head.next
+		before.next = None
+		sofar = 0
+
+		while True:
+			tmp = None
+			if after.next:
+				tmp = after.next
+			after.next = before
+
+			sofar += 1
+			if sofar == k:
+				pass
+
+			before = after
+			if tmp is None:
+				break
+			else:
+				after = tmp
+		return after
+
+
+	def reverseKGroup(self, head, k):
+		"""
+		:type head: ListNode
+		:type k: int
+		:rtype: ListNode
+		"""
+		start = head
+		total = 0
+		while start:
+			total += 1
+			start = start.next
+		print total
+
+		iterate = total / k
+
+		head = head
+		first, last = None, None
+		last_round_last = None
+
+		for i in range(iterate):
+			before, after = head, head.next
+			before.next = None
+			before_cp = before
+			sofar = 1
+			while True:
+				if sofar == k:
+					break
+
+				tmp = None
+				if after.next:
+					tmp = after.next
+				after.next = before
+
+				sofar += 1
+
+				before = after
+				if tmp is None:
+					break
+				else:
+					after = tmp
+
+			if i == 0:
+				first = before
+
+			if last_round_last:
+				last_round_last.next = before
+
+			last_round_last = before_cp
+			head = after
+		last_round_last.next= head
+
+		return first
+
+
+l = ListNode(1)
+s = l
+for i in range(2, 10):
+	l.next = ListNode(i)
+	l = l.next
+
+solution = Solution()
+solution.print_all(s)
+
+s = solution.reverseKGroup(s, 3)
+solution.print_all(s)
+
+
