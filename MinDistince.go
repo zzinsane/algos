9266d62f7fef351a3677df53f443f247053217c2
Zhen Zhu <zhuz@Garenas-MacBook-Pro-2.local>
2017-05-24 15:15:16 +0800

    mindistince between strings final commit

--- MinDistince.go
+++ MinDistince.go
@@ -8,7 +8,25 @@ import (
 var (
 	word1IdxMapping = make(map[int][]int)
 	cache = make(map[string]int)
+	len1 = 0
+	len2 = 0
+
 )
+
+func stretch(word1, word2 string, start1, start2 int, end1, end2 int) (int) {
+
+	length := 0
+	for {
+		idx1 := start1 + length
+		idx2 := start2 + length
+
+		if idx1 < end1 && idx2 < end2 && word1[idx1] == word2[idx2]{
+			length += 1
+		} else {
+			return length
+		}
+	}
+}
 
 
 func minDistinceWrapper(word1, word2 string, start1, start2, end1, end2 int) int {
@@ -20,21 +38,38 @@ func minDistinceWrapper(word1, word2 string, start1, start2, end1, end2 int) int
 	current := int(math.Max(float64(end1 - start1), float64(end2 - start2)))
 
 	if start1 >= end1 || start2 >= end2 {
+
 		return current
 	}
 
-	for idx, _ := range word1[start1:end1] {
+	for idxI, _ := range word1[start1:end1] {
+		idx := idxI + start1
 		idxArray := word1IdxMapping[idx]
 		for _, word2Idx := range idxArray {
-			if word2Idx >= start2 && word2Idx < end2{
+
+			if word2Idx >= start2 && word2Idx < end2 {
+				stretchLen := stretch(word1, word2, idx, word2Idx, end1, end2)
+
+				start21 := idx + stretchLen
+				start22 := word2Idx + stretchLen
+
+				minimum := math.Abs(float64((idx - start1) - (word2Idx - start2))) + math.Abs(float64((end1 - start21)- (end2 - start22)))
+				if int(minimum) >= current {
+					continue
+				}
+
 
 				v1 := minDistinceWrapper(word1, word2, start1, start2, idx, word2Idx)
-				v2 := minDistinceWrapper(word1, word2, idx + 1, word2Idx + 1, end1, end2)
+
+				if v1 > current {
+					continue
+				}
+
+				v2 := minDistinceWrapper(word1, word2, start21, start22, end1, end2)
 
 
 				if (v1+v2) < current {
 
-					//fmt.Println(idx, word2Idx, v1, v2)
 
 					current = v1 + v2
 				}
@@ -44,6 +79,7 @@ func minDistinceWrapper(word1, word2 string, start1, start2, end1, end2 int) int
 
 	cache[fmt.Sprintf("%d_%d-%d_%d", start1, end1, start2, end2)] = current
 
+
 	return current
 }
 
@@ -52,6 +88,8 @@ func minDistance(word1 string, word2 string) (result int) {
 
 	word1IdxMapping = make(map[int][]int)
 	cache = make(map[string]int)
+	len1 = len(word1)
+	len2 = len(word2)
 
 	for idx1, c1 := range word1 {
 		idxArray := make([]int, 0)
@@ -64,16 +102,9 @@ func minDistance(word1 string, word2 string) (result int) {
 	}
 
 	return minDistinceWrapper(word1, word2, 0, 0, len(word1), len(word2))
-
 }
 
 func main() {
-	//fmt.Println(minDistance("aswqfer", "dewrec"))
-	//fmt.Println(minDistance("jdewkfnd32324jswqdnqirqrh", "csdnfiewhr232e23i94jwsjdidnd"))
-
-	fmt.Println(minDistance(
-		"abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdef",
-		"bcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefg"))
-
-	fmt.Println(minDistance("a", "b"))
+	fmt.Println(minDistance("adewd3efqwiedeiquwfoerbfrebfibewofeqr epqwhdewuqh zwewjrew zrfeuturegt", "bjdiewnqfdefefnvfrbgbfebfrefbvfevbfriwugbeife dweidewfhure"))
+	fmt.Println(minDistance("teacher", "reteaches"))
 }
